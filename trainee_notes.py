b02ce3e4944575a78ab78d44f95bcf924de6c1fc
Qin Yong <172900H@mymail.nyp.edu.sg>
2017-12-14 15:01:26 +0800

    unknown stuff removed

--- trainee_notes.py
+++ trainee_notes.py
@@ -1,3 +1,20 @@
-from scaledrone import ScaleDrone
+class comment:
+    def __init__(self, name, comment):
+        self.__name = name
+        self.__comment = comment
 
-drone = ScaleDrone('SNazg8KrKdwSphWf', 'fCw1xxKBLoYBFZuif4vRKgK3ibIdH6mk')
+    # Accessors
+
+    def get_name(self):
+        return self.__name
+
+    def get_comment(self):
+        return self.__comment
+
+    # Mutators
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_comment(self, comment):
+        self.__comment = comment
