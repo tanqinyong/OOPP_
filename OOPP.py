85250e5a2a7d3983937f97eb2b60f556516ca98e
Qin Yong <172900H@mymail.nyp.edu.sg>
2018-01-30 19:41:30 +0800

    debugging heroku

--- OOPP.py
+++ OOPP.py
@@ -18,7 +18,7 @@ from trainee_notes import comment
 # Download the twilio-python library from twilio.com/docs/libraries/python
 from twilio.rest import Client
 # Find these values at https://twilio.com/user/account
-account_sid = "AP6544e558798642dff01770950db780ce"
+account_sid = "AC6ced1d481c8e1d8ec33c4f0da613e3e8"
 auth_token = "5554f393e7cf77b1496cb9f2de0d61e2"
 client = Client(account_sid, auth_token)
 
