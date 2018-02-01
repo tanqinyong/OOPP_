382c0b474d773311b3885e850da6a0dae01b2200
Qin Yong <172900H@mymail.nyp.edu.sg>
2018-02-01 10:16:24 +0800

    date & menu

--- Food_Order.py
+++ Food_Order.py
@@ -1,8 +1,13 @@
 class FoodOrder:
-    def __init__(self, foodname, days, patient_id):
+    def __init__(self, foodname, days, patient_id, indian, malay, chinese, western, international):
         self.__foodname = foodname
         self.__days = days
         self.__patient_id = patient_id
+        self.__indian = indian
+        self.__malay = malay
+        self.__chinese = chinese
+        self.__western = western
+        self.__international = international
 
     # Accessor methods
     def get_foodname(self):
@@ -14,6 +19,21 @@ class FoodOrder:
     def get_patient_id(self):
         return self.__patient_id
 
+    def get_indian(self):
+        return self.__indian
+
+    def get_malay(self):
+        return self.__malay
+
+    def get_chinese(self):
+        return self.__chinese
+
+    def get_western(self):
+        return self.__western
+
+    def get_international(self):
+        return self.__international
+
     # Mutator methods
     def set_foodname(self,foodname):
         self.__foodname = foodname
@@ -23,5 +43,22 @@ class FoodOrder:
 
     def set_patient_id(self,patient_id):
         self.__patient_id = patient_id
+
+    def set_indian(self,indian):
+        self.__indian = indian
+
+    def set_malay(self,malay):
+        self.__malay = malay
+
+    def set_chinese(self,chinese):
+        self.__chinese = chinese
+
+    def set_western(self,western):
+        self.__western = western
+
+    def set_international(self,international):
+        self.__international = international
+
+
 
 
