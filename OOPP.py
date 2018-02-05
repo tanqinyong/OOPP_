54adab9e7d9edb3fd16ac3b8da33a06031521d44
Qin Yong <172900H@mymail.nyp.edu.sg>
2018-02-05 16:36:24 +0800

    twilio

--- OOPP.py
+++ OOPP.py
@@ -19,11 +19,11 @@ import json
 # TWILIO
 # /usr/bin/env python
 # Download the twilio-python library from twilio.com/docs/libraries/python
-from twilio.rest import Client
-# Find these values at https://twilio.com/user/account
-account_sid = "AC6ced1d481c8e1d8ec33c4f0da613e3e8"
-auth_token = "5554f393e7cf77b1496cb9f2de0d61e2"
-client = Client(account_sid, auth_token)
+# from twilio.rest import Client
+# # Find these values at https://twilio.com/user/account
+# account_sid = "AC6ced1d481c8e1d8ec33c4f0da613e3e8"
+# auth_token = "5554f393e7cf77b1496cb9f2de0d61e2"
+# client = Client(account_sid, auth_token)
 
 
 UPLOAD_FOLDER = 'static/images/'
@@ -322,10 +322,10 @@ def render_admin():
                 setid.set_patient_id(data)
                 print(data)
             flash(new_patient.get_name() +' added!(Patient)'+ ' User = '+username + ' Password = '+password, 'success')
-            client.api.account.messages.create(
-                to="+6592211065",
-                from_="+18636927542",
-                body="Your user is: {} and password: {}".format(username,password))
+            # client.api.account.messages.create(
+            #     to="+6592211065",
+            #     from_="+18636927542",
+            #     body="Your user is: {} and password: {}".format(username,password))
 
     return render_template('Admin.html',form=admin_form)
 
